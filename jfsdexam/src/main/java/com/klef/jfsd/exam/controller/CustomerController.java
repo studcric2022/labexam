37181ce879b60c9da26ca06eb335b38b5651637e
studcric2022 <rohitkumarsingh2021@gmail.com>
2024-12-07 10:45:46 +0530

    exam completed
    exam completed with execution

---
+++ jfsdexam/src/main/java/com/klef/jfsd/exam/controller/CustomerController.java
@@ -0,0 +1,25 @@
+
+package com.klef.jfsd.exam.controller;
+
+import com.klef.jfsd.exam.model.Customer;
+
+import com.klef.jfsd.exam.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/customers")
+public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable Long id,
+                                                    @RequestParam String name,
+                                                    @RequestParam String address) {
+        Customer updatedCustomer = customerService.updateCustomer(id, name, address);
+        return ResponseEntity.ok(updatedCustomer);
+    }
+}
